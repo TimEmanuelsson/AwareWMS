f50bc144e3ed9b0a507d32acd9ba3836170c9c96
as223jx <as223jx@student.lnu.se>
2015-05-05 12:32:46 +0200

    Börjat kika på exceptionlog

--- Server/AwareServer/AwareServer/InputHandler.cs
+++ Server/AwareServer/AwareServer/InputHandler.cs
@@ -25,36 +25,23 @@ namespace AwareServer
                     if (content.IndexOf("GET/orders/id=") > -1)
                     {
                         string id = content.Replace("GET/orders/id=", "");
-                        //Repository.Model.Order order = service.GetOrderById(1);
-                        AwareClassLibrary.Order order = service.GetOrderById(int.Parse(id));
-                        //string ret = String.Format("ID: {0} Customer ID: {1} Date: {2} Last update: {3} Payment status: {4} Payment method: LÄGG TILL DÅ"
-                        //    , order.OrderId, order.CustomerId, order.Date, order.LastUpdate, order.PaymentStatus);
-                        JavaScriptSerializer serializer = new JavaScriptSerializer();
-                        ret += serializer.Serialize(order);
+                        Order order = service.GetOrderById(int.Parse(id));
+                        ret = JsonConvert.SerializeObject(order);
                     }
 
                     else if (content.IndexOf("GET/orders") > -1)
                     {
-                        IEnumerable<AwareClassLibrary.Order> orders = service.GetOrders();
-
-                        int i = 0;
-
-                        foreach (AwareClassLibrary.Order order in orders)
+                        IEnumerable<Order> orders = service.GetOrders();
+                        foreach (Order order in orders)
                         {
-                            if (i > 0)
-                            {
-                                ret += "";
-                            }
-                            JavaScriptSerializer serializer = new JavaScriptSerializer();
-                            ret += serializer.Serialize(order);
-
-                            i++;
+                            ret = JsonConvert.SerializeObject(orders);
                         }
-
                     }
                     else
                     {
                         // TODO: Kasta exception
+                        //ExceptionLog exceptionLog = new ExceptionLog(0, "Type", "Invalid call string from the client", "", "");
+                        //service.InsertException(exceptionLog);
                         ret = "An error has occured! Format errors in the call to the server.";
                     }
                 }
@@ -64,48 +51,28 @@ namespace AwareServer
                     if (content.IndexOf("GET/products/id=") > -1)
                     {
                         string id = content.Replace("GET/products/id=", "");
-                        AwareClassLibrary.Product product = service.GetProductById(int.Parse(id));
-                        //string ret = String.Format("ID: {0} Name: {1} SKU: {2} Quantity: {3} Weight: {4} Shelf: {5}  Barcode Number: {6} Image URL: {7}"
-                        //    , product.ProductId, product.Name, product.SKU, product.Quantity, product.Weight, product.StorageSpace, product.BarcodeNumber, product.ImageLocation);
-                        JavaScriptSerializer serializer = new JavaScriptSerializer();
-                        ret += serializer.Serialize(product);
+                        Product product = service.GetProductById(int.Parse(id));
+                        ret = JsonConvert.SerializeObject(product);
                     }
                     else if (content.IndexOf("GET/products/sku=") > -1)
                     {
                         string sku = content.Replace("GET/products/sku=", "");
-                        AwareClassLibrary.Product product = service.GetProductBySKU(int.Parse(sku));
-                        JavaScriptSerializer serializer = new JavaScriptSerializer();
-                        ret += serializer.Serialize(product);
-                        //GetProductBySKU(int.Parse(sku), handler);
+                        Product product = service.GetProductBySKU(int.Parse(sku));
+                        ret = JsonConvert.SerializeObject(product);
                     }
                     else if (content.IndexOf("GET/products/barcodenumber=") > -1)
                     {
                         string barcodenumber = content.Replace("GET/products/barcodenumber=", "");
-                        AwareClassLibrary.Product product = service.GetProductByBarcodeNumber(int.Parse(barcodenumber));
-                        JavaScriptSerializer serializer = new JavaScriptSerializer();
-                        ret += serializer.Serialize(product);
+                        Product product = service.GetProductByBarcodeNumber(int.Parse(barcodenumber));
+                        ret = JsonConvert.SerializeObject(product);
                     }
                     else if (content.IndexOf("GET/products") > -1)
                     {
-                        IEnumerable<AwareClassLibrary.Product> products = service.GetProducts();
+                        IEnumerable<Product> products = service.GetProducts();
 
-                        int i = 0;
-
-                        foreach (AwareClassLibrary.Product product in products)
+                        foreach (Product product in products)
                         {
-                            /*
-                            if (i > 0)
-                            {
-                                ret += "";
-                            }
-                            JavaScriptSerializer serializer = new JavaScriptSerializer();
-                            ret += serializer.Serialize(product);
-
-                            i++;
-                             * */
-
                             ret = JsonConvert.SerializeObject(products);
-                                
                         }
 
                     }
@@ -122,9 +89,8 @@ namespace AwareServer
                     if (content.IndexOf("GET/customers/id=") > -1)
                     {
                         string id = content.Replace("GET/customers/id=", "");
-                        AwareClassLibrary.Customer customer = service.GetCustomerById(int.Parse(id)) as AwareClassLibrary.Customer;
-                        JavaScriptSerializer serializer = new JavaScriptSerializer();
-                        ret += serializer.Serialize(customer);
+                        Customer customer = service.GetCustomerById(int.Parse(id));
+                        ret += JsonConvert.SerializeObject(customer);
                     }
                     //else if (content.IndexOf("name") > -1)
                     //{

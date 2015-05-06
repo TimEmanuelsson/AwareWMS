879dbd9b353aba033b1afda6f6c264b326c61355
Joakim <jn222na@student.lnu.se>
2015-05-06 15:31:35 +0200

    ListLoopFanKlar!: !! !!

--- Datorklient/AwareComputerClient/AwareComputerClient/View/MainView.cs
+++ Datorklient/AwareComputerClient/AwareComputerClient/View/MainView.cs
@@ -19,21 +19,56 @@ namespace AwareComputerClient.View
             InitializeComponent();
         }
         private List<Product> products;
-        private void button1_Click(object sender, EventArgs e)
+        private void ShowProducts_Click(object sender, EventArgs e)
         {
             AsynchronousClient async = new AsynchronousClient();
             string response = async.Response;
-            
+            TableView.Visible = true;
             products = JsonConvert.DeserializeObject<List<Product>>(response);
-            
+
             foreach (Product item in products)
             {
-                
+
                 TableView.AutoSize = true;
                 var bindinglist = new BindingList<Product>(products);
                 var source = new BindingSource(bindinglist, null);
                 TableView.DataSource = source;
             }
+            
+            
+        }
+
+
+        private void FileMenuContainer_Click(object sender, EventArgs e)
+        {
+            //När File klickas på
+        }
+
+        private void FileMenuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        private string s;
+
+        private void TableView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var cellRowIndex = TableView.SelectedCells[0].RowIndex;
+            int cellColumnIndex = TableView.ColumnCount -1;
+            var cellcollection = TableView.Rows[cellRowIndex].Cells[0];
+
+
+            for (int i = 0; i <= cellColumnIndex; i++)
+            {
+                s += TableView.Rows[cellRowIndex].Cells[i].Value.ToString();
+
+            }
+            MessageBox.Show(s.ToString());
+            s = "";
         }
 
     }

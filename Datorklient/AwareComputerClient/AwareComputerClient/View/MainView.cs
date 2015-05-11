d496b4d6f1a1d64d4e6807e1f2bba416b88c8c2e
Joakim <jn222na@student.lnu.se>
2015-05-11 23:24:03 +0200

    Editering slutförd, filtrering uppdaterad, sortering påbörjad

--- Datorklient/AwareComputerClient/AwareComputerClient/View/MainView.cs
+++ Datorklient/AwareComputerClient/AwareComputerClient/View/MainView.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using AwareComputerClient.Model;
 using Newtonsoft.Json;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace AwareComputerClient.View
 {
@@ -19,47 +21,66 @@ namespace AwareComputerClient.View
         {
             InitializeComponent();
         }
-        private AsynchronousClient async;
+        private AsynchronousClient async = new AsynchronousClient();
         private List<Product> products;
         private BindingList<Product> bindinglist;
+        private Timer checkForChanged;
+        private string savedJsonString = null;
+        private string response = null;
         private void ShowProducts_Click()
         {
-            async = new AsynchronousClient();
-           // ShowProductsButton.Enabled = false;
-           // TableView.ColumnHeaderMouseClick
+
+            // ShowProductsButton.Enabled = false;
+            // TableView.ColumnHeaderMouseClick
             TableViewPanel.Visible = true;
             ShowProductsMainContainer.Visible = true;
-            async.StartClient("GET/products");
-            string response = async.Response;
             TableView.Visible = true;
-            products = JsonConvert.DeserializeObject<List<Product>>(response);
-
             TableView.AutoSize = true;
-
-        
-                bindinglist = new BindingList<Product>(products);
-                var source = new BindingSource(bindinglist, null);
-                TableView.DataSource = source;
+            TableView.ColumnHeadersVisible = false;
+            checkForChanged = new Timer();
+            checkForChanged.Interval = 1000;
+            checkForChanged.Tick += new EventHandler(updateList);
+            checkForChanged.Start();
             
+            if (savedJsonString == null)
+            {
+                async.StartClient("GET/products");
+                response = async.Response;
+                savedJsonString = response;
 
+                products = JsonConvert.DeserializeObject<List<Product>>(response);
+                renewDataSource();
                 
-            //TableView.ColumnHeadersVisible = false;
+            }
 
-            //TableView.Columns[0].Name = "column0";
-            //TableView.Columns[1].Name = "column1";
-            //TableView.Columns[2].Name = "column2";
-            //TableView.Columns[3].Name = "column3";
-            //TableView.Columns[4].Name = "column4";
-            //TableView.Columns[5].Name = "column5";
-            //TableView.Columns[6].Name = "column6";
-            // row1 = new string[] { "row1", "row2", "row3", "row4", "row5", "row6", "row7" };
-            //TableView.Rows.Add(row1);
-            //TableView.Visible = true;
         }
-            //private string[] row1;
-        
-        
 
+        private void updateList(object sender, EventArgs e)
+        {
+            async.StartClient("GET/products");
+            response = async.Response;
+
+            if (savedJsonString != null)
+            {
+                if (!response.Equals(savedJsonString))
+                {
+                    savedJsonString = response;
+                    products = JsonConvert.DeserializeObject<List<Product>>(response);
+                    renewDataSource();
+                }
+            }
+        }
+        private void renewDataSource()
+        {
+
+            bindinglist = new BindingList<Product>(products);
+            var source = new BindingSource(bindinglist, null);
+            
+            TableView.DataSource = source;
+
+            TableView.ClearSelection();
+            TableView.Rows[cellRowIndex].Selected = true;
+        }
         private void FileMenuContainer_Click(object sender, EventArgs e)
         {
             //När File klickas på
@@ -74,37 +95,44 @@ namespace AwareComputerClient.View
         {
 
         }
-           
-        private void TableView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private int cellRowIndex;
+        private void TableView_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            int cellRowIndex = TableView.SelectedCells[0].RowIndex;
-            int cellColumnIndex = TableView.ColumnCount -1;
-           // var cellcollection = TableView.Rows[cellRowIndex].Cells[0];  
-            ProductIdLabel.Text = TableView.Rows[cellRowIndex].Cells[0].Value.ToString();
-            NameLabel.Text = TableView.Rows[cellRowIndex].Cells[1].Value.ToString();
-            SKULabel.Text = TableView.Rows[cellRowIndex].Cells[2].Value.ToString();
-            QuantityLabel.Text = TableView.Rows[cellRowIndex].Cells[3].Value.ToString();
-            WeightLabel.Text = TableView.Rows[cellRowIndex].Cells[4].Value.ToString();
-            StorageSpaceLabel.Text = TableView.Rows[cellRowIndex].Cells[5].Value.ToString();
-            BarcodeNumberLabel.Text = TableView.Rows[cellRowIndex].Cells[6].Value.ToString();
-            ImageLocationLabel.Text = TableView.Rows[cellRowIndex].Cells[7].Value.ToString();
-            LastInventoryLabel.Text = TableView.Rows[cellRowIndex].Cells[8].Value.ToString();
             
-            
+
+            //Gets our current selected rowcell
+            cellRowIndex = TableView.SelectedCells[0].RowIndex;
+            TableView.Rows[cellRowIndex].Selected = true;
+            int cellColumnIndex = TableView.ColumnCount - 1;
+            // var cellcollection = TableView.Rows[cellRowIndex].Cells[0];  
+    // ProductIdLabel.Text = TableView.Rows[cellRowIndex].Cells[0].Value.ToString();
+            NameLabel.Text     = TableView.Rows[cellRowIndex].Cells[0].Value.ToString();
+            SKULabel.Text      = TableView.Rows[cellRowIndex].Cells[1].Value.ToString();
+            QuantityLabel.Text = TableView.Rows[cellRowIndex].Cells[2].Value.ToString();
+            WeightLabel.Text   = TableView.Rows[cellRowIndex].Cells[3].Value.ToString();
+            StorageSpaceLabel.Text  = TableView.Rows[cellRowIndex].Cells[4].Value.ToString();
+            BarcodeNumberLabel.Text = TableView.Rows[cellRowIndex].Cells[5].Value.ToString();
+            ImageLocationLabel.Text = TableView.Rows[cellRowIndex].Cells[6].Value.ToString();
+    //LastInventoryLabel.Text = TableView.Rows[cellRowIndex].Cells[8].Value.ToString();
         }
         //TODO:: All Edit Stora Bokstäver
         private void SearchField_TextChanged(object sender, EventArgs e)
         {
-            //Looks using the Product object class, if it contains any character withing the searchfield, filter.
-            BindingList<Product> filtered = new BindingList<Product>(products.Where(p => p.Name.Contains(SearchField.Text)).ToList());
+            //Capitalizes All Words In A Sentence
+            string capitalizedSearchBoxText = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(SearchField.Text);
+         
+            //Looks using the Product object class, if it contains any character within the searchfield filter.
+            BindingList<Product> filtered = new BindingList<Product>(products.Where(p => p.Name.Contains(capitalizedSearchBoxText)).ToList());
+
+            
             TableView.DataSource = filtered;
         }
-        
+
         protected override void ShowProductsButton_Click(object sender, EventArgs e)
         {
             ShowProducts_Click();
         }
-        
+
         protected override void ViewOrders_Click(object sender, EventArgs e)
         {
             //OrderView s = new OrderView();
@@ -115,48 +143,57 @@ namespace AwareComputerClient.View
 
         protected override void Stocktaking_Click(object sender, EventArgs e)
         {
-           // ShowProductsButton.Enabled = true;
+            // ShowProductsButton.Enabled = true;
         }
-        Product editProduct = new Product();
 
-        
-  
         private void SaveEditButton_Click(object sender, EventArgs e)
-        {                       
-            //editProduct.Name = NameLabel.Text;
-            //editProduct.SKU = SKULabel.Text;
-            //editProduct.Quantity = int.Parse(QuantityLabel.Text);
-            //editProduct.Weight = double.Parse(WeightLabel.Text);
-            //editProduct.StorageSpace = StorageSpaceLabel.Text;
-            //editProduct.BarcodeNumber = BarcodeNumberLabel.Text;
-            //int cellRowIndex = TableView.SelectedCells[0].RowIndex;
-            //products.Add(editProduct);
-            //TableView.Rows[cellRowIndex].Cells[0].Value = ProductIdLabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[1].Value = NameLabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[2].Value = SKULabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[3].Value = QuantityLabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[4].Value = WeightLabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[5].Value = StorageSpaceLabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[6].Value = BarcodeNumberLabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[7].Value = ImageLocationLabel.Text;
-            //TableView.Rows[cellRowIndex].Cells[8].Value = LastInventoryLabel.Text;
+        {
 
-            async = new AsynchronousClient();
-            //MessageBox.Show(jsonString);
-
-            var bindinglist = new BindingList<Product>(products);
-            var source = new BindingSource(bindinglist, null);
-            TableView.DataSource = source;
-           
             Product currentobject = (Product)TableView.CurrentRow.DataBoundItem;
+            currentobject.Name = NameLabel.Text;
+            currentobject.SKU = SKULabel.Text;
             currentobject.Quantity = Convert.ToInt32(QuantityLabel.Text);
-
-             
-
-                //bindinglist.SingleOrDefault();
-                //MessageBox.Show(bindinglist.ToString());
-                string serializedObject = JsonConvert.SerializeObject(currentobject);
-                async.StartClient("PUT/products/inventory/json=" + serializedObject);
+            currentobject.Weight = Convert.ToDouble(WeightLabel.Text);
+            currentobject.StorageSpace = StorageSpaceLabel.Text;
+             renewDataSource();
+            string serializedObject = JsonConvert.SerializeObject(currentobject);
+            async.StartClient("PUT/products/json=" + serializedObject);
         }
+
+  
     }
 }
+
+
+
+//TableView.ColumnHeadersVisible = false;
+
+//TableView.Columns[0].Name = "column0";
+//TableView.Columns[1].Name = "column1";
+//TableView.Columns[2].Name = "column2";
+//TableView.Columns[3].Name = "column3";
+//TableView.Columns[4].Name = "column4";
+//TableView.Columns[5].Name = "column5";
+//TableView.Columns[6].Name = "column6";
+// row1 = new string[] { "row1", "row2", "row3", "row4", "row5", "row6", "row7" };
+//TableView.Rows.Add(row1);
+//TableView.Visible = true;
+//private string[] row1;
+
+//editProduct.Name = NameLabel.Text;
+//editProduct.SKU = SKULabel.Text;
+//editProduct.Quantity = int.Parse(QuantityLabel.Text);
+//editProduct.Weight = double.Parse(WeightLabel.Text);
+//editProduct.StorageSpace = StorageSpaceLabel.Text;
+//editProduct.BarcodeNumber = BarcodeNumberLabel.Text;
+//int cellRowIndex = TableView.SelectedCells[0].RowIndex;
+//products.Add(editProduct);
+//TableView.Rows[cellRowIndex].Cells[0].Value = ProductIdLabel.Text;
+//TableView.Rows[cellRowIndex].Cells[1].Value = NameLabel.Text;
+//TableView.Rows[cellRowIndex].Cells[2].Value = SKULabel.Text;
+//TableView.Rows[cellRowIndex].Cells[3].Value = QuantityLabel.Text;
+//TableView.Rows[cellRowIndex].Cells[4].Value = WeightLabel.Text;
+//TableView.Rows[cellRowIndex].Cells[5].Value = StorageSpaceLabel.Text;
+//TableView.Rows[cellRowIndex].Cells[6].Value = BarcodeNumberLabel.Text;
+//TableView.Rows[cellRowIndex].Cells[7].Value = ImageLocationLabel.Text;
+//TableView.Rows[cellRowIndex].Cells[8].Value = LastInventoryLabel.Text;
